48597cc2ef006b0f1e5186599fe59ff86ff78eef
humandotlearning <humandotlearning@gmail.com>
2021-04-06 17:29:18 +0530

    + basic templates

--- app/route.py
+++ app/route.py
@@ -1,16 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user= {'username': 'miguel'}
-    return '''
-    <html>
-        <head>
-            <title> hello world </title>
-        </head>
-        <body>
-            <h1> hello ''' + user['username'] + ''' </h1>
-        </body>
-    </html>
-    '''
+    return render_template("index.html", title="Home" , user= user)
