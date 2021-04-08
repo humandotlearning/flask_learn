ad291635c26e4e63ae7783eb18385aa78c20ade5
humandotlearning <humandotlearning@gmail.com>
2021-04-08 15:46:15 +0530

    + database

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 
 # Flask uses the location of the module passed here as a starting point when it needs to load associated resources such as template files
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
 # The bottom import is a workaround to circular imports, 
 # a common problem with Flask applications.
 # You are going to see that the routes module needs to import the app variable defined in this script, 
 # so putting one of the reciprocal imports at the bottom avoids the error that results from the mutual references between these two files.
-from app import route
+# models: defines the structure of the flask db
+from app import route, models
--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
 
+base_pth = os.path.abspath( os.path.dirname(__file__))
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or "you-will-never-guess"
+
+    # database
+    SQL_ALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(base_pth, 'app.db')
+
+    # feature of Flask-SQLAlchemy that I do not need, which is to signal the application every time a change is about to be made in the database.
+    SQLALCHEMY_TRACK_MODIFICATION = False
