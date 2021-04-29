626c119f59d39a4a4b79ae7f622850519c25e40a
humandotlearning <humandotlearning@gmail.com>
2021-04-29 22:09:31 +0530

    + comment heading to each file

--- app/route.py
+++ app/route.py
@@ -1,3 +1,10 @@
+##################################################################################
+
+            # defining the website paths 
+
+########################################################################################
+
+
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
